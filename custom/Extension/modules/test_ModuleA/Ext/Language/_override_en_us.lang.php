11dbe38f0453bab4fae0180727bb0f5c8d37f020
dineshitkar18 <dineshitkar18@gmail.com>
2026-06-29 20:44:31 +0530

    CR-101 - Change module desc label

---
+++ custom/Extension/modules/test_ModuleA/Ext/Language/_override_en_us.lang.php
@@ -0,0 +1,3 @@
+<?php
+// created: 2026-06-29 17:12:03
+$mod_strings['LBL_DESCRIPTION'] = 'Description Edited';
---
+++ custom/modules/test_ModuleA/Ext/Language/en_us.lang.ext.php
@@ -0,0 +1,7 @@
+<?php
+// WARNING: The contents of this file are auto-generated
+
+
+// created: 2026-06-29 17:12:03
+$mod_strings['LBL_DESCRIPTION'] = 'Description Edited';
+
