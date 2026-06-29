6f527ad89537c60ee7f24e0aae825cafe36d0c34
dineshitkar18 <dineshitkar18@gmail.com>
2026-06-29 17:58:34 +0530

    CR-101 - Change test_ModuleA module description label

---
+++ custom/Extension/modules/test_ModuleA/Ext/Language/_override_en_us.lang.php
@@ -0,0 +1,3 @@
+<?php
+// created: 2026-06-29 14:27:17
+$mod_strings['LBL_DESCRIPTION'] = 'Description Edited';
---
+++ custom/modules/test_ModuleA/Ext/Language/en_us.lang.ext.php
@@ -0,0 +1,7 @@
+<?php
+// WARNING: The contents of this file are auto-generated
+
+
+// created: 2026-06-29 14:27:17
+$mod_strings['LBL_DESCRIPTION'] = 'Description Edited';
+
