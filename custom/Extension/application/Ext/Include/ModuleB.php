6645a73f227dfd17f11927670ed64d0afd806ddb
dineshitkar18 <dineshitkar18@gmail.com>
2026-06-30 13:55:27 +0530

    Create Module B

---
+++ custom/Extension/application/Ext/Include/ModuleB.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['test_ModuleB'] = 'test_ModuleB';
+$beanFiles['test_ModuleB'] = 'modules/test_ModuleB/test_ModuleB.php';
+$moduleList[] = 'test_ModuleB';
+
+?>
